ee6ae44d11661fcaf126ac0c643b7f789544f05b
Process <felix.douaud@epitech.eu>
2024-09-12 17:56:49 +0200

    a

--- script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-document.addEventListener('DOMContentLoaded', function() {
-    const nav = document.querySelector('nav ul');
-    const toggleMenu = document.querySelector('.menu-toggle');
-
-    toggleMenu.addEventListener('click', function() {
-        nav.classList.toggle('active');
-    });
-});
